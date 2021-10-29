01d8b13ff605d947c89c9f7e2a1179f6b111e1ab
Adrian Curless <awcurless@wpi.edu>
2021-10-29 09:55:32 -0400

    focused_ksm: store page pointer in metadata struct

--- kernel/focused_ksm.h
+++ kernel/focused_ksm.h
@@ -9,9 +9,8 @@ typedef struct list_head* sus_metadata_collection_t;
 
 struct metadata_collection
 {
-    size_t page_start;
-    size_t page_end;
     u8 checksum[SHA3_512_LEN];
+    struct page* page;
 };
 
 int sus_mod_merge(unsigned long pid1, unsigned long pid2);
