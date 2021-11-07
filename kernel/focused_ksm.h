041637f964389c03e82eaf5045a5b777d93196a8
Alex Simoneau <afsimoneau@gmail.com>
2021-11-07 17:26:52 -0500

    hashing using address from kmap_atomic

--- kernel/focused_ksm.h
+++ kernel/focused_ksm.h
@@ -1,15 +1,14 @@
 #ifndef _FOCUSED_KSM_H
 #define _FOCUSED_KSM_H
 
-#define SHA3_512_LEN (512 / 8)
-
+#include <crypto/sha3.h>
 #include <linux/list.h>
 
 typedef struct list_head* sus_metadata_collection_t;
 
 struct metadata_collection
 {
-    u8 checksum[SHA3_512_LEN];
+    u8 checksum[SHA3_512_DIGEST_SIZE];
     struct page* page;
     struct list_head list;
 };
