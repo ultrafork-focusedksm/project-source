5f271e94204dfb52d0f99b1132c1358acc4b604f
Alex Simoneau <afsimoneau@gmail.com>
2021-11-08 09:02:24 -0500

    reallocating and freeing hasing objects

--- kernel/focused_ksm.c
+++ kernel/focused_ksm.c
@@ -18,9 +18,6 @@ static struct task_struct* find_task_from_pid(unsigned long pid)
     return get_pid_task(pid_struct, PIDTYPE_PID);
 }
 
-static struct crypto_shash* tfm; // hash transform object
-static struct shash_desc* desc;
-
 static int callback_pte_range(pte_t* pte, unsigned long addr,
                               unsigned long next, struct mm_walk* walk)
 {
@@ -29,22 +26,31 @@ static int callback_pte_range(pte_t* pte, unsigned long addr,
     // TODO: check page flags
     if (true)
     {
-        u8 digest_out[SHA3_512_DIGEST_SIZE];
+        struct crypto_shash* tfm; // hash transform object
+        struct shash_desc* desc;
 
-        void* addr = kmap_atomic(page); // address to page
-        crypto_shash_digest(desc, addr, PAGE_SIZE, digest_out);
-        kunmap_atomic(addr)
+        // TODO: error check tfm
+        tfm = crypto_alloc_shash("sha3-512", 0, 0); // init transform object
+        desc = kmalloc(sizeof(*desc) + crypto_shash_descsize(tfm),
+                       GFP_KERNEL); // init descriptor object
+        desc->tfm = tfm; // set descriptor transform object for our hashing call
 
         struct metadata_collection* new_meta =
             kmalloc(sizeof(*new_meta), GFP_KERNEL);
-        new_meta->checksum = *digest_out; // need to change from array of u8 to
-                                          // pointer for digest?
+
         new_meta->page = current_page;
+
+        void* addr = kmap_atomic(page); // address to page
+        //ignore huge pages
+        crypto_shash_digest(desc, addr, PAGE_SIZE, new_meta->checksum);
+        kunmap_atomic(addr);
 
         // access metadata_list pointer via walk->private
         // https://stackoverflow.com/questions/33933344/adding-items-to-a-linux-kernel-linked-list
         // check robert love book for list API
         list_add(&new_meta->list, walk->private);
+        kfree(tfm);
+        kfree(desc);
         return 0; // TODO: add proper return
     }
     return -1;
@@ -56,20 +62,11 @@ static sus_metadata_collection_t traverse(unsigned long pid)
 {
     struct task_struct* task = find_task_from_pid(pid); // get task struct
 
-    // TODO: get help with dumb pointers for metadata collection list thing
     sus_metadata_collection_t metadata_list =
         kmalloc(sizeof(metadata_list), GFP_KERNEL); // output list
     LIST_HEAD(metadata_list);                       // initialize list
-
-    tfm = crypto_alloc_shash("sha3-512", 0, 0); // init transform object
-
-    desc = kmalloc(sizeof(*desc) + crypto_shash_descsize(tfm),
-                   GFP_KERNEL); // init descriptor object
-    desc->tfm = tfm; // set descriptor transform object for our hashing call
-
+    
     // TODO: hold @mm->mmap_lock?
-    // TODO: walk through multiple vm_area_structs? is mmap a list? what about
-    // no_vma? note that the pointer to metadata_list is passed in void *private
     walk_page_range(task->mm, 0, TASK_SIZE, &task_walk_ops, &metadata_list);
 
     return metadata_list;
