bfcbc62844c215d7d00943f54722101374ca7fc6
Adrian Curless <awcurless@wpi.edu>
2021-11-09 12:20:14 -0500

    ultrafork: iterate through children of the given pid

--- kernel/ultrafork.c
+++ kernel/ultrafork.c
@@ -84,6 +84,7 @@ static struct task_struct* find_task_from_pid(unsigned long pid)
 
 int sus_mod_fork(unsigned long pid, unsigned char flags)
 {
+    struct list_head* list;
     struct task_struct* parent;
 
     if (pid < 1)
@@ -98,6 +99,13 @@ int sus_mod_fork(unsigned long pid, unsigned char flags)
         return -EINVAL;
     }
 
-    walk_task(parent, NULL, &rtask_logger);
+    pr_info(UFRK_LOG_PREFIX " %s pid=%d, tgid=%d\n", parent->comm, parent->pid,
+            parent->tgid);
+    list_for_each(list, &parent->children)
+    {
+        struct task_struct* task =
+            list_entry(list, struct task_struct, sibling);
+        pr_info("%s, pid=%d, tgid=%d\n", task->comm, task->pid, task->tgid);
+    }
     return 0;
 }
