2f91fa2b1dd39a20b481a12a74f4af4177ce6435
Alex Simoneau <afsimoneau@gmail.com>
2021-11-10 09:23:18 -0500

    todos

--- kernel/focused_ksm.h
+++ kernel/focused_ksm.h
@@ -8,10 +8,7 @@ typedef struct list_head* sus_metadata_collection_t;
 
 struct metadata_collection
 {
-<<<<<<< HEAD
-=======
     u8 checksum[SHA3_512_DIGEST_SIZE];
->>>>>>> b2ec342 (hashing using address from kmap_atomic)
     struct page* page;
     struct list_head list;
     u8 checksum[SHA3_512_DIGEST_SIZE];
