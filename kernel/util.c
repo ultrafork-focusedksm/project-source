7d9f1256132fa740d43ff53f16af1104c0c6f945
Adrian Curless <awcurless@wpi.edu>
2022-02-08 12:19:05 -0500

    ultrafork: add timer code

--- kernel/util.c
+++ kernel/util.c
@@ -5,3 +5,15 @@ struct task_struct* find_task_from_pid(pid_t pid)
     struct pid* pid_struct = find_get_pid(pid);
     return get_pid_task(pid_struct, PIDTYPE_PID);
 }
+
+u64 sus_time_jiffies(void)
+{
+    return get_jiffies_64();
+}
+
+struct timespec64 sus_micro_difference(u64 large_jiffers, u64 small_jiffies)
+{
+    struct timespec64 ts;
+    jiffies_to_timespec64(large_jiffers - small_jiffies, &ts);
+    return ts;
+}
--- kernel/util.h
+++ kernel/util.h
@@ -2,8 +2,24 @@
 #define _UTIL_H
 
 #include <linux/sched/task.h>
+#include <linux/time.h>
 #include <linux/types.h>
 
 struct task_struct* find_task_from_pid(pid_t pid);
+
+/**
+ *  Returns the jiffy count. Used to compute time taken to execute
+ *  functions.
+ */
+u64 sus_time_jiffies(void);
+
+/**
+ * Computes the difference between the two jiffy counts.
+ *
+ * difference = large_jiffies - small_jiffies
+ *
+ * @return timespec64 containing the difference.
+ */
+struct timespec64 sus_micro_difference(u64 large_jiffers, u64 small_jiffies);
 
 #endif
